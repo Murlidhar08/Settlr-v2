20ed9a9f0ab5a041bb0798619534ed66ad9e4e74
Murlidhar Properties <murlidharproperties08@gmail.com>
2026-05-28 10:59:04 +0530

    Initial commit

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,54 @@
+import { redirect } from "next/navigation";
+
+export const dynamic = "force-dynamic";
+
+// Lib
+import { getUserSession } from "@/lib/auth/auth";
+import { isSetupRequired } from "@/lib/setup";
+
+// Components
+import { ImpersonationIndicator } from "@/components/auth/impersonation-indicator";
+import { LayoutTransitions } from "@/components/layout-transitions";
+import { NavBar } from "@/components/navbar/nav-bar";
+import { UserConfigProvider } from "@/components/providers/user-config-provider";
+import { getDefaultConfig, getUserConfig } from "@/lib/user-config";
+import { UserStatus } from "@/lib/generated/prisma/enums";
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  // Check if setup is needed
+  if (await isSetupRequired()) {
+    redirect("/setup" as any);
+  }
+
+  // User Config
+  const userConfig = await getUserConfig() ?? getDefaultConfig()
+  const session = await getUserSession();
+  const status = session.user.status;
+
+  // Handle Redirection based on status
+  if (session.user.banned) redirect("/banned");
+  if (status === UserStatus.pendingapproval) redirect("/pending-approval");
+  if (status === UserStatus.suspended) redirect("/suspended");
+
+  return (
+    <UserConfigProvider config={userConfig}>
+      <div className="h-screen bg-background text-foreground transition-colors duration-300 overflow-hidden">
+        <div className="flex h-full">
+          <NavBar />
+
+          {/* Main Content */}
+          <div className="flex-1 flex flex-col min-w-0 h-full overflow-y-auto overflow-x-hidden">
+            <LayoutTransitions>
+              {children}
+            </LayoutTransitions>
+          </div>
+        </div>
+
+        {/* Only when admin is impersonating */}
+        <ImpersonationIndicator />
+      </div>
+
+    </UserConfigProvider >
+  );
+}
+
---
+++ resources/helpfile.md
@@ -0,0 +1,15 @@
+# Build with env file
+`docker compose --env-file .env.production up --build -d`
+
+
+# Add upstream repo
+`git remote add upstream https://github.com/Murlidhar08/next-auth-template.git`
+
+# Disable remote branch push
+`git remote set-url --push upstream DISABLE`
+`git remote -v`
+
+# Add from upstream
+`git fetch upstream`
+`git merge upstream/main --allow-unrelated-histories`
+`git push origin main`
